094abbd80787dba8b73fc9aa585df978095783ea
kamgru <kamil.grudzien@hotmail.com>
2024-07-26 22:25:46 +0200

    nvim zen mode config update

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
@@ -28,12 +29,28 @@ vim.call('plug#end')
 
 vim.cmd('colorscheme kanagawa')
 
-vim.keymap.set('n', '<leader>zz', '<cmd>ZenMode<cr>')
+-- zen mode config
+zen_opt = {
+	plugins = {
+		options = {
+			enabled = true,
+			showcmd = true,
+			laststatus = 3
+		},
+		tmux = { enabled = true }
+	}
+}
+local zen = require("zen-mode");
+vim.keymap.set('n', '<leader>zz', function() zen.toggle(zen_opt) end)
+-- zen mode config end
+
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-vim.keymap.set('n', '<leader>nn', '<cmd>noh<cr>');
+vim.keymap.set('n', '<leader>nn', '<cmd>noh<cr>')
+vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.rename() end)
+vim.keymap.set('n', '<leader>qwe', '<cmd>e ~/.config/nvim/init.lua<cr>')
 
 require'lspconfig'.clangd.setup{}
 
